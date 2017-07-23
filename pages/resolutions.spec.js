50add1ca5460bc9976e5f591bc020745b288619b
Emiliano Errecalde <eerrecalde@gmail.com>
2017-07-23 14:23:13 +0100

    Unit test bug fix.

--- pages/resolutions.spec.js
+++ pages/resolutions.spec.js
@@ -1,6 +1,6 @@
 // Import Vue and the component being tested
 import Vue from 'vue'
-import Home from './index.vue'
+import Home from './resolutions.vue'
 // Here are some Jasmine 2.0 tests, though you can
 // use any test runner / assertion library combo you prefer
 describe('Home', () => {
